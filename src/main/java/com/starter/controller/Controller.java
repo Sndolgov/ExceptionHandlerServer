a4a71aaeb4eb8aa5daaeda1e8f0801596a8ff8bd
sndolgov <sndolgov84@gmail.com>
2021-01-10 20:53:17 +0200

    added @CustomController

--- src/main/java/com/starter/controller/Controller.java
+++ src/main/java/com/starter/controller/Controller.java
@@ -1,12 +1,12 @@
 package com.starter.controller;
 
+import com.starter.CustomController;
 import com.starter.service.PSRService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@CustomController
 @RequestMapping("psr/")
 public class Controller
 {
